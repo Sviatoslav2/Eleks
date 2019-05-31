0954ff208073a854f4883748f3b8c3d6dde839ad
Sviatoslav2 <fedoriv@ucu.edu.ua>
2019-05-31 20:28:33 +0300

    Add files via upload

---
+++ data_calculation.py
@@ -0,0 +1,22 @@
+import numpy as np
+def get_projection_number(expiration_date, valuation_date=[2019,    1,   31]):
+    return (expiration_date[0] - valuation_date[0]) * 12
+
+def get_monthly_payment(records, idx, prog, cur_sum ,lim=700000, size=100):
+    SEED  = records.data['user_id'][idx]
+    np.random.seed(SEED)
+    mu, sigma = (prog+1)/100 ,0.1
+    if lim > cur_sum:
+        return np.random.lognormal(mu, sigma, size)
+    else:
+        return np.array([0.0001 for i in range(100)])
+
+def get_payment(prev, gender, payment_rate):
+    if gender == 1:
+        mortality = 1.01
+    elif gender == 0:
+        mortality = 0.99
+    return prev *  payment_rate  * mortality              
+    
+def loan(curent_sum, prev, gender, payment_rate ):
+    return np.float64(curent_sum + get_payment(prev, gender, payment_rate))    
